348e55b61c405929b08638234b712006743c1296
AdrashX <mariogamerhome2@gmail.com>
2025-04-24 12:01:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "20346550"))
-API_HASH = getenv("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
-BOT_TOKEN = getenv("BOT_TOKEN", "7358697038:AAGJBd7zlkB66QnP_4XHD3Ww17ymxIUonq8")
-OWNER_ID = int(getenv("OWNER_ID", "7081036509"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7081036509 7491167754").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "13460015"))
+API_HASH = getenv("API_HASH", "bd452af645d2a569f654f70b366ce577")
+BOT_TOKEN = getenv("BOT_TOKEN", "7581825946:AAHzGwkqx4Rs-4Dx7pjZaq52SQOJ60H6cWs")
+OWNER_ID = int(getenv("OWNER_ID", "6749313249"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6749313249 5431970720").split()))
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://mariogamerhome:z2nQreOIbwy9OzgR@cluster0.lqfpvlc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002278731669"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002278731669"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002640138333"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002640138333"))
 
 """
 #
